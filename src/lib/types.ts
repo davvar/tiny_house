6edde3c5a95d840f4b0a32f77f93ed88db036a4a
David Vardanyan <davvar40@gmail.com>
2020-12-27 00:19:44 +0400

    setup-done

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,17 +1,64 @@
-import { Collection, ObjectID } from 'mongodb'
+import { Collection, ObjectId, ObjectID } from 'mongodb'
+
+export enum ListingType {
+	Apartment = 'APARTMENT',
+	House = 'HOUSE',
+}
+
+export interface IBookingIndexMonth {
+	[key: string]: boolean
+}
+
+export interface IBookingIndexYear {
+	[month: string]: IBookingIndexMonth
+}
+
+export interface IBookingIndex {
+	[year: string]: IBookingIndexYear
+}
+
+export interface IBooking {
+	_id: ObjectID
+	listing: ObjectId
+	tenant: string
+	checkIn: string
+	checkOut: string
+}
 
 export interface Listing {
 	_id: ObjectID
 	title: string
+	description: string
 	image: string
+	host: string
+	type: ListingType
 	address: string
+	country: string
+	admin: string
+	city: string
+	bookings: ObjectId[]
+	bookingsIndex: IBookingIndex
 	price: number
 	numOfGuests: number
-	numOfBeds: number
-	numOfBaths: number
-	rating: number
+	// numOfBeds: number
+	// numOfBaths: number
+	// rating: number
+}
+
+export interface IUser {
+	_id: string
+	token: string
+	name: string
+	avatar: string
+	contact: string
+	walletId?: string
+	income: number
+	bookings: ObjectId[]
+	listings: ObjectId[]
 }
 
 export interface Database {
 	listings: Collection<Listing>
+	users: Collection<IUser>
+	bookings: Collection<IBooking>
 }
