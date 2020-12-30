9638a61796c2b5d6f2d9f8c8ffebca5f11cca3a3
David Vardanyan <davvar40@gmail.com>
2020-12-30 23:10:06 +0400

    user page

---
+++ src/graphql/resolvers/Bookings/index.ts
@@ -0,0 +1,15 @@
+import { IResolvers } from 'apollo-server-express'
+import { IBooking, IDatabase, IListing } from '../../../lib/types'
+
+export const bookingResolvers: IResolvers = {
+	Booking: {
+		id: (booking: IBooking): string => booking._id.toString(),
+		listing: (
+			booking: IBooking,
+			_args: Record<string, never>,
+			{ db }: { db: IDatabase }
+		): Promise<IListing | null> => {
+			return db.bookings.findOne({ _id: booking.listing })
+		},
+	},
+}
--- src/graphql/resolvers/index.ts
+++ src/graphql/resolvers/index.ts
@@ -1,5 +1,12 @@
 import { merge } from 'lodash'
+import { bookingResolvers } from './Bookings'
+import { listingResolvers } from './Listings'
 import { userResolver } from './User'
 import { viewerResolver } from './Viewer'
 
-export const resolvers = merge(viewerResolver, userResolver)
+export const resolvers = merge(
+	viewerResolver,
+	userResolver,
+	listingResolvers,
+	bookingResolvers
+)
