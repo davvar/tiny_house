f766177a691d35cdf7798a087ce55589c3c77931
David Vardanyan <davvar40@gmail.com>
2021-01-03 18:37:02 +0400

    added filterinf location with google maps api

--- src/lib/api/Google.ts
+++ src/lib/api/Google.ts
@@ -1,10 +1,16 @@
-import { google, people_v1 } from 'googleapis'
+import { AddressComponent, createClient } from '@google/maps';
+import { google, people_v1 } from 'googleapis';
 
 const auth = new google.auth.OAuth2(
 	process.env.G_CLIENT_ID,
 	process.env.G_CLIENT_SECRET,
 	`${process.env.PUBLIC_URL}/login`
 )
+
+const maps = createClient({
+	key: `${process.env.G_GEOCODE_API}`,
+	Promise,
+})
 
 export const Google = {
 	authUrl: auth.generateAuthUrl({
@@ -25,4 +31,39 @@ export const Google = {
 
 		return data
 	},
+
+	geocode: async (address: string) => {
+		const { status, json } = await maps.geocode({ address }).asPromise()
+
+		if (status < 200 || status > 299) {
+			throw new Error('failed to geocode address')
+		}
+
+		return parseAddress(json.results[0].address_components)
+	},
+}
+
+function parseAddress(addressComponents: AddressComponent<unknown>[]) {
+	let country = null
+	let city = null
+	let admin = null
+
+	addressComponents.forEach(component => {
+		if (component.types.includes('country')) {
+			country = component.long_name
+		}
+
+		if (component.types.includes('administrative_area_level_1')) {
+			admin = component.long_name
+		}
+
+		if (
+			component.types.includes('locality') ||
+			component.types.includes('postal_town')
+		) {
+			city = component.long_name
+		}
+	})
+
+	return { admin, city, country }
 }
