8fb3f3c96d68a930cfa5b2bfca72b8994ac782f0
David Vardanyan <davvar40@gmail.com>
2021-02-05 02:36:14 +0400

    .

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,38 @@
-import { ApolloServer } from 'apollo-server-express'
-import compression from 'compression'
-import cookieParser from 'cookie-parser'
-import express, { Application } from 'express'
-import { express as voyagerMiddleware } from 'graphql-voyager/middleware'
-import { connectDatabase } from './database'
-import { resolvers, typeDefs } from './graphql'
-import { IContext, IDatabase } from './typings'
+import { ApolloServer } from 'apollo-server-express';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import express, { Application } from 'express';
+import { express as voyagerMiddleware } from 'graphql-voyager/middleware';
+import { connectDatabase } from './database';
+import { resolvers, typeDefs } from './graphql';
+import { IContext, IDatabase } from './typings';
 
 const mount = async (app: Application) => {
-	const db: IDatabase = await connectDatabase()
+	try {
+		const db: IDatabase = await connectDatabase()
 
-	app.use(express.json({ limit: '2mb' }))
-	app.use(cookieParser(process.env.SECRET))
-	app.use('/voyager', voyagerMiddleware({ endpointUrl: '/api' }))
-	app.use(compression())
+		app.use(express.json({ limit: '2mb' }))
+		app.use(cookieParser(process.env.SECRET))
+		app.use('/voyager', voyagerMiddleware({ endpointUrl: '/api' }))
+		app.use(compression())
 
-	app.use(express.static(`${__dirname}/client`))
-	app.get('/*', (_req, res) => res.sendFile(`${__dirname}/client/index.html`))
+		app.use(express.static(`${__dirname}/client`))
+		app.get('/*', (_req, res) => res.sendFile(`${__dirname}/client/index.html`))
 
-	const server = new ApolloServer({
-		typeDefs,
-		resolvers,
-		context: ({ req, res }): IContext => ({ db, req, res }),
-	})
+		const server = new ApolloServer({
+			typeDefs,
+			resolvers,
+			context: ({ req, res }): IContext => ({ db, req, res }),
+		})
 
-	server.applyMiddleware({ app, path: '/api' })
+		server.applyMiddleware({ app, path: '/api' })
 
-	app.listen(process.env.PORT)
-	console.log(`[app]: http://localhost:${process.env.PORT}`)
+		app.listen(process.env.PORT)
+		console.log(`[app]: http://localhost:${process.env.PORT}`)
+
+	} catch (error) {
+		console.log({ error });
+	}
 }
 
 mount(express())
